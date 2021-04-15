e2cec4dd6d45ece50e5c5f3f097aa3db1219dcd0
Anna <annamoraco@gmail.com>
2021-04-15 17:46:08 -0300

    Adicionando níveis de dificuldade

--- advinhacao.c
+++ advinhacao.c
@@ -10,8 +10,25 @@ int main(){
     srand(s);
     int numerosecreto = rand()%10;
     int numerotentativas = 20;
+    int chute = 0;
+    int nivel;
     
+    printf("Selecione o nível de dificuldade desejado: \n");
+    printf("(1) Fácil   (2) Médio   (3) Difí­cil \n");
+    scanf("%d",&nivel);
+
     printf("%d \n",numerosecreto);
+
+    printf("Digite um número maior que zero: ");
+    scanf("%d",&chute);
+
+    if (chute == numerosecreto)
+    {
+        printf("Parabéns, você ganhou!!!");
+    }
+    
+
+    
 
 
 }
