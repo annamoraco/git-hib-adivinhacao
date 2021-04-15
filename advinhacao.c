0c00ba239552cb344bf5da9faa079ba52d91cdf4
Anna <annamoraco@gmail.com>
2021-04-15 15:11:51 -0300

    Alterando a semente do rand

--- advinhacao.c
+++ advinhacao.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main(){
 
+    int s = time(NULL);
+    srand(s);
     int numerosecreto = rand()%10;
     int numerotentativas = 20;
     
