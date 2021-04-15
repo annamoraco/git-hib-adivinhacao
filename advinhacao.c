10052fdb333d6b35ce655ec59988e6824d576afb
Anna <annamoraco@gmail.com>
2021-04-15 18:45:59 -0300

    Jogo pronto

--- advinhacao.c
+++ advinhacao.c
@@ -8,12 +8,12 @@ int main(){
     setlocale(LC_ALL, "Portuguese");
     int s = time(NULL);
     srand(s);
-    int numerosecreto = rand()%10;
+    int numerosecreto = rand()%100;
     int numerotentativas = 20;
-    int chute = 0;
+    int chute = -1;
     int nivel, tentativa = 1;
     
-    printf("Bem vindo ao jogo de adivinhação!!! \n\n");
+    printf("\n\nBem vindo ao jogo de adivinhação!!! \n\n");
 
     printf("Selecione o nível de dificuldade desejado: \n");
     printf("(1) Fácil   (2) Médio   (3) Difí­cil \n");
@@ -34,22 +34,40 @@ int main(){
 
     printf("%d \n",numerosecreto);
 
-    printf("Tentativa %d de %d\n",tentativa,numerotentativas);
-    printf("Digite um número maior que zero: ");
-    scanf("%d",&chute);
+    while (tentativa <= numerotentativas)
+    {
+        printf("\nTentativa %d de %d\n",tentativa,numerotentativas);
+        printf("Digite um número maior ou igual zero: ");
+        scanf("%d",&chute);
+        if (chute < 0)
+        {
+            printf("\nVocê não pode digitar números negativos.\n");
+            continue;
+        }
 
-    if (chute == numerosecreto)
+        if (chute == numerosecreto)
+        {
+            break;
+        }
+        else if (chute < numerosecreto)
+        {
+            printf("\nVocê errou.\nO número secreto é maior.\n");
+        }
+        else
+        {
+            printf("\nVocê errou.\nO número secreto é menor.\n");
+        }
+
+        tentativa++;
+    }    
+
+    if (tentativa <= numerotentativas)
     {
         printf("\n\nParabéns, você ganhou!!!\n\n");
     }
-    else if (chute < numerosecreto)
+    else 
     {
-        printf("\nVocê errou.\nO número secreto é maior.\n");
+        printf("\n\nVocê perdeu, tente mais uma vez.\n\n");
     }
-    else
-    {
-        printf("\nVocê errou.\nO número secreto é menor.\n");
-    }
-    
 
 }
