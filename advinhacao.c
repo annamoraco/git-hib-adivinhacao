e85514437827c1de43740b662931e41b27d107a4
Anna <annamoraco@gmail.com>
2021-04-15 17:36:18 -0300

    Configurando localidade/encoding para acentuação

--- advinhacao.c
+++ advinhacao.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <locale.h>
 
 int main(){
 
+    setlocale(LC_ALL, "Portuguese");
     int s = time(NULL);
     srand(s);
     int numerosecreto = rand()%10;
